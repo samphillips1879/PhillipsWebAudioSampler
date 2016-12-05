b7209cd9f773ed8f44b15c166d939910368ae230
Samuel Phillips <samphillips1879@gmail.com>
2016-12-05 10:51:18 -0600

    angular and firebase initialized

--- app/app.js
+++ app/app.js
@@ -1,2 +1,46 @@
 "use strict";
-let app = angular.module("app", []);
+
+//establishing angular app
+let app = angular.module("WebSamplerApp", ["ngRoute"]);
+
+
+let isAuth = (AuthFactory)=> new Promise((resolve,reject)=>{
+    AuthFactory.isAuthenticated()
+    .then((userExists)=>{
+        if (userExists) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+});
+
+
+
+
+// app.config(($routeProvider)=>{
+//     $routeProvider
+//     .when('/items/list', {
+//         templateUrl: 'partials/item-list.html',
+//         controller: 'ItemListCtrl',
+//         resolve: {isAuth}
+//     })
+//     .when('/items/:itemId', {
+//         templateUrl: 'partials/item-details.html',
+//         controller: 'ItemViewCtrl',
+//         resolve: {isAuth}
+//     })
+//     .otherwise('/items/list');
+// });
+
+
+
+
+app.run(($location, FBCreds)=>{
+    let authConfig = {
+        apiKey: FBCreds.key,
+        authDomain: FBCreds.authDomain
+    };
+    firebase.initializeApp(authConfig);
+});
+
---
+++ app/values/FBCreds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.constant("FBCreds", {
+	key: "AIzaSyDR8I0_bBtgYyekWH3MOhx6zf-01Eb2OFU",
+	authDomain: "phillipswebaudiosampler.firebaseapp.com",
+	URL: "https://phillipswebaudiosampler.firebaseio.com"
+});
