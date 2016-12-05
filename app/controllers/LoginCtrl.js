72cd139b39e1ebe899eae1119b9f036d92e67e67
Samuel Phillips <samphillips1879@gmail.com>
2016-12-05 15:33:34 -0600

    initialized database, getting started with patch values structure

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -21,7 +21,10 @@ app.controller("LoginCtrl", function($scope, AuthFactory, $window){
 		AuthFactory.loginUser($scope.account)
 		.then((user)=>{
 			$window.location.href = "#/play";
-			console.log("current user", user.uid);
+			AuthFactory.isAuthenticated() //THIS SHOULD BE UNNECESSARY, WAS JUST INCLUDED FOR MY SAKE IN SEEING IF getUser() WAS WORKING
+			.then(()=>{
+				console.log("AuthFactory.getUser()", AuthFactory.getUser());
+			});
 		});
 	};
 });
