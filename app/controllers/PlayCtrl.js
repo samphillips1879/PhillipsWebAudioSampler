88ce06b465e5714ecaf5a1bf20b62cb8542a6c04
Samuel Phillips <samphillips1879@gmail.com>
2016-12-08 10:36:25 -0600

    more cleaning

--- app/controllers/PlayCtrl.js
+++ app/controllers/PlayCtrl.js
@@ -1,8 +1,8 @@
 "use strict";
-app.controller("PlayCtrl", function($scope, PatchSamples, PatchFactory){
+app.controller("PlayCtrl", function($scope, PatchFactory){
 	$scope.greeting = "Play Controller Connected";
 
-	console.log("PatchSamples", PatchSamples);
+	// console.log("PatchSamples", PatchSamples);
 
 //creating the audio context
 	const AUD_CTX = new window.AudioContext();
@@ -12,9 +12,9 @@ app.controller("PlayCtrl", function($scope, PatchSamples, PatchFactory){
 
 //patch initialization
 	$scope.patch = PatchFactory.getCurrentPatch();
-	console.log("$scope.patch", $scope.patch); //setting the patch to either the default template or whatever the user has saved as their currentPatch
+	// console.log("$scope.patch", $scope.patch); //setting the patch to either the default template or whatever the user has saved as their currentPatch
 	let patch = $scope.patch; //this is to normalize the following logic, so that what follows can be stored outside of anything involving $scope
-	console.log("patch", patch);
+	// console.log("patch", patch);
 
 	//connecting paths
 	patch.channels.forEach((channel, channelNumber)=>{
