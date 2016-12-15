b53b484c71cb8cbade4a8f50cb34fcffcb09a794
Samuel Phillips <samphillips1879@gmail.com>
2016-12-15 10:02:44 -0600

    cleaned up PlayCtrl and play.html

--- app/controllers/PlayCtrl.js
+++ app/controllers/PlayCtrl.js
@@ -1,120 +1,64 @@
 "use strict";
 app.controller("PlayCtrl", function($scope, PatchFactory){
 	$scope.greeting = "Play Controller Connected";
-
-	// console.log("PatchSamples", PatchSamples);
-
-
-
-
-// ************************
-//section moved to app.js 
-//creating the audio context
-	// const AUD_CTX = new window.AudioContext();
-	//including webkit version, because jshint didn't like it but I want to save it to try and get to work later
-	// const AUD_CTX = new (window.AudioContext || window.webkitAudioContext)();
-// ************************
-
+	let chan = null;
 
 //patch initialization
 	$scope.patch = PatchFactory.currentPatch;
 	// console.log("$scope.patch", $scope.patch); //setting the patch to either the default template or whatever the user has saved as their currentPatch
-	let patch = $scope.patch; //this is to normalize the following logic, so that what follows can be stored outside of anything involving $scope
-	// console.log("patch", patch);
+	let patch = $scope.patch; 
 
 	//connecting paths
 	patch.channels.forEach((channel, channelNumber)=>{
-
 		//create those nodes, homeskillet
 		channel.gain = AUD_CTX.createGain();
 		channel.gain.gain.value = channel.gainValue;
-		// console.log(`channel ${channel.channelNum}`, channel);
-		
 		// connect them paths, yo
 		channel.gain.connect(AUD_CTX.destination);
-
 	});
-
-
-
-
-
 
 	$scope.playSample = (channelNumber)=>{
 		console.log(`playing sample at chan ${channelNumber}`);
-
-	 	if(patch.channels[channelNumber].sampleSource){
-	 		patch.channels[channelNumber].sampleSource.stop();
-	 		patch.channels[channelNumber].sampleSource = null;
+		chan = patch.channels[channelNumber];
+	 	if(chan.sampleSource){
+	 		chan.sampleSource.stop();
+	 		chan.sampleSource = null;
 	 	}
-	 	// let sampleSourceNode = AUD_CTX.createBufferSource();
-	 	// sampleSourceNode.buffer = bufferToBeAssigned;
-
-
-
-	 	patch.channels[channelNumber].sampleSource = AUD_CTX.createBufferSource();
-	 	patch.channels[channelNumber].sampleSource.buffer = patch.channels[channelNumber].sampleBuffer;
-
-	 	
-	 	patch.channels[channelNumber].sampleSource.connect(patch.channels[channelNumber].gain);
-	 	patch.channels[channelNumber].sampleSource.start();
+	 	chan.sampleSource = AUD_CTX.createBufferSource();
+	 	chan.sampleSource.buffer = chan.sampleBuffer;
+	 	chan.sampleSource.connect(chan.gain);
+	 	chan.sampleSource.start();
 	};
 	$scope.stopSample = (channelNumber)=>{
 		console.log(`stopping sample at chan ${channelNumber}`);
-		patch.channels[channelNumber].sampleSource.stop();
-		patch.channels[channelNumber].sampleSource = null;
+		chan = patch.channels[channelNumber];
+		chan.sampleSource.stop();
+		chan.sampleSource = null;
 	};
-
-	
-
-
-
-
-
-
-//osc version
-
-	// $scope.playOsc = (channelNumber)=>{
-	// 	if(patch.channels[channelNumber].sourceOsc){
-	// 		patch.channels[channelNumber].sourceOsc.stop();
-	// 		patch.channels[channelNumber].sourceOsc = null;
-	// 	}
-	// 	patch.channels[channelNumber].sourceOsc = AUD_CTX.createOscillator();
-	// 	patch.channels[channelNumber].sourceOsc.connect(patch.channels[channelNumber].gain);
-	// 	patch.channels[channelNumber].sourceOsc.start();
-	// };
-	// $scope.stopOsc = (channelNumber)=>{
-	// 	console.log("stopping an osc");
-	// 	patch.channels[channelNumber].sourceOsc.stop();
-	// 	patch.channels[channelNumber].sourceOsc = null;
-	// };
-
-	
-
 
 	//to bring in a new patch, set an event that, once triggered, sets $scope.patch to whatever patch is saved in PatchFactory
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
