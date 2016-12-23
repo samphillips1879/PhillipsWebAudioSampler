7b3f5208723a8d4a91625eaa46786f171d6fd6fc
Samuel Phillips <samphillips1879@gmail.com>
2016-12-23 11:11:28 -0600

    SampleFactory Created

---
+++ app/factories/SampleFactory.js
@@ -0,0 +1,51 @@
+"use strict";
+
+app.factory("SampleFactory", ()=>{
+	let SampleFactory = {};
+	
+	SampleFactory.channels = [
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 0
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 1
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 2
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 3
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 4
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 5
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 6
+			},
+			{
+				sampleSource: null,
+				sampleBuffer: null,
+				channelNum: 7
+			}
+		];
+	
+
+	return SampleFactory;
+});
