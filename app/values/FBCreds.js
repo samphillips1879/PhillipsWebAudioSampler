d2fecd8e8d56978d9677177aae5e5eaac1fe698c
samphillips1879 <samphillips1879@gmail.com>
2017-02-18 09:45:20 -0600

    Delete FBCreds.js

--- app/values/FBCreds.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-app.constant("FBCreds", {
-	key: "AIzaSyDR8I0_bBtgYyekWH3MOhx6zf-01Eb2OFU",
-	authDomain: "phillipswebaudiosampler.firebaseapp.com",
-	URL: "https://phillipswebaudiosampler.firebaseio.com",
-	storageBucket: "phillipswebaudiosampler.appspot.com"
-});
